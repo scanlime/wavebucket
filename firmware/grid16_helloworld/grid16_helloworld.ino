b80e17aba235a4794df04681536ed9f6115ba099
Micah Elizabeth Scott <beth@scanlime.org>
2013-03-19 16:56:17 -0700

    Dithering, fuck yeah!

--- firmware/grid16_helloworld/grid16_helloworld.ino
+++ firmware/grid16_helloworld/grid16_helloworld.ino
@@ -45,28 +45,66 @@ void setup()
   leds.begin();
 }
 
+struct DitheredLED {
+  float color[3];
+  float residual[3];
+};
+
+DitheredLED dither[ledCount];
+
 void loop()
 {
-  // Background
+  // Background and fade.
   for (unsigned i = 0; i < ledCount; ++i) {
     Point p = i;
     unsigned dist = grid.distance(p, center);
-    leds.setPixel(i, lerp(0x000000, 0x200820, (sin(dist * 0.2 + millis() * 0.001) + 1) * 127));
+    float alpha = 0.5 * (sin(dist * 0.4 + millis() * 0.002) + 1);
+    dither[i].color[0] = dither[i].color[0] * 0.9 + alpha * 0.0125;
+    dither[i].color[1] = dither[i].color[1] * 0.9 + alpha * 0.0031;
+    dither[i].color[2] = dither[i].color[2] * 0.9 + alpha * 0.0125;
   }
 
-  // Wanderer
-  leds.setPixel(p, 0xffffff);
-  if (++counter == 100) {
+  dither[p].color[1] += 0.2;
+  if (++counter == 8) {
     counter = 0;
-    grid.move(p, random(1, 4));
+
+    // Move in the dimmest direction
+    Point best = Point::invalid();
+    float bestMetric = 1e10;
+    Point nbr[3];
+    unsigned nbrCount = grid.neighbors(p, nbr);
+    for (unsigned i = 0; i < nbrCount; ++i) {
+      float metric = dither[nbr[i]].color[0] + dither[nbr[i]].color[1] + dither[nbr[i]].color[2];
+      if (metric < bestMetric) {
+        bestMetric = metric;
+        best = nbr[i];
+      }
+    }
+
+    if (best != Point::invalid()) {
+      p = best;
+    }
   }
 
-  // Shape
-  Point z;
-  if ((z = grid.next(p, A_AXIS)) != Point::invalid()) leds.setPixel(z, 0xff0000);
-  if ((z = grid.next(p, B_AXIS)) != Point::invalid()) leds.setPixel(z, 0x00ff00);
-  if ((z = grid.next(p, C_AXIS)) != Point::invalid()) leds.setPixel(z, 0x0000ff);
+  // LED dithering
+  for (unsigned i = 0; i < ledCount; ++i) {
+    float fr = dither[i].color[0] + dither[i].residual[0];
+    float fg = dither[i].color[1] + dither[i].residual[1];
+    float fb = dither[i].color[2] + dither[i].residual[2];
+    int r = fr * 255 + 0.5;
+    int g = fg * 255 + 0.5;
+    int b = fb * 255 + 0.5;
+    if (r < 0) r = 0;
+    if (r > 255) r = 255;
+    if (g < 0) g = 0;
+    if (g > 255) g = 255;
+    if (b < 0) b = 0;
+    if (b > 255) b = 255;
+    dither[i].residual[0] = fr - (r / 255.0);
+    dither[i].residual[1] = fg - (g / 255.0);
+    dither[i].residual[2] = fb - (b / 255.0);
+    leds.setPixel(i, r, g, b);
+  }
 
   leds.show();
-  delay(5);
 }
